0415c7af577b55e0d8fb1014dd69cc3ad2f19052
sunofbeat <sunmam964@naver.com>
2022-07-22 17:41:03 +0900

    '0722'

---
+++ workspace/MyBatis/src/ch02/domain/User.java
@@ -0,0 +1,24 @@
+package ch02.domain;
+
+import java.time.LocalDate;
+
+//과제 users relation과 mapping 할 User object를 디자인하라.
+public class User {
+	private int userId;
+	private String userName;
+	private LocalDate regDate;
+	
+	public User(int userId, String userName, LocalDate regDate) {
+		this.userName = userName;
+		this.userId = userId;
+		this.regDate = regDate;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return String.format("%d %s %s", userId, userName, regDate);
+	}
+
+}
